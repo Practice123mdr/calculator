8269cfb9e4faf4c4e31adbf9264036ec2abffa75
oui <triggerlesgolems00@gmail.com>
2022-10-23 17:40:22 +0200

    added files

---
+++ script.js
@@ -0,0 +1,144 @@
+const buttonsDigit = document.querySelectorAll(".button-digit");
+const buttonsOperator = document.querySelectorAll(".button-operator");
+const buttonDelete = document.getElementsByClassName("button-delete");
+const buttonsDigitArray = Array.from(buttonsDigit);
+const buttonsOperatorArray = Array.from(buttonsOperator);
+const buttonDeleteArray = Array.from(buttonDelete);
+
+const displayScreen = document.getElementById("display-screen");
+const displayScreenArray = Array.from(displayScreen);
+
+let temp;
+let tempOperator;
+let tempResult;
+displayScreen.innerText = "0";
+let firstValue = ["0"];
+let secondValue = ["0"];
+let currentOperator;
+let result = 0;
+
+function reset() {
+    firstValue = ["0"];
+    secondValue = ["0"];
+    currentOperator = undefined;
+    result = 0;
+    displayScreen.innerText = "0";
+    return;
+}
+
+
+function equals(a, b, ope) {
+    if (ope === "+") {
+        console.log(a + b)
+        result = a + b;
+        if (isNaN(result) === true) {
+            return;
+        }
+        return firstValue = result;
+    } else if (ope === "-") {
+        console.log(a - b)
+        result = a - b;
+        if (isNaN(result) === true) {
+            return;
+        }
+        return firstValue = result;
+    } else if (ope === "*") {
+        console.log(a * b)
+        result = a * b;
+        if (isNaN(result) === true) {
+            return;
+        }
+        return firstValue = result;
+    } else if (ope === "÷") {
+        console.log(a / b)
+        result = a / b;
+        if (isNaN(result) === true) {
+            return;
+        }
+        return firstValue = result;
+    } else if (ope === "=") {
+        if (currentOperator = "+") {
+            console.log(a + b)
+            result = a + b;
+            if (isNaN(result) === true) {
+                return;
+            }
+            return firstValue = result;
+        } else if (currentOperator === "-") {
+            console.log(a - b)
+            result = a - b;
+            if (isNaN(result) === true) {
+                return;
+            }
+            return firstValue = result;
+        } else if (currentOperator === "*") {
+            console.log(a * b)
+            result = a * b;
+            if (isNaN(result) === true) {
+                return;
+            }
+            return firstValue = result;
+        } else if (currentOperator === "÷") {
+            console.log(a / b)
+            result = a / b;
+            if (isNaN(result) === true) {
+                return;
+            }
+            return firstValue = result;
+        }
+    }
+}
+
+buttonsDigit.forEach((element) =>  {
+    element.addEventListener("click", () => {
+        if (typeof(firstValue) === "object" && currentOperator === undefined) {
+            if (element.value === ".") {
+                if (firstValue.includes(".", 0)) {
+                    return;
+                }
+            }
+        } if (currentOperator != undefined && typeof(secondValue) === "object") {
+            if (element.value === ".") {
+                if (secondValue.includes(".", 0)) {
+                    return;
+                }
+            }
+        }
+        if (currentOperator === undefined) {
+            firstValue.push(element.value);
+        } if (currentOperator != undefined) {
+            secondValue.push(element.value);
+        } 
+    });
+});
+
+buttonsOperatorArray.forEach((element => {
+    element.addEventListener("click", () => {
+        if (currentOperator === undefined) {
+            return currentOperator = element.value;
+        }
+        if (element.value === "clear") {
+            return reset();
+        }
+        if (firstValue.length === 0 && secondValue.length === 0 && currentOperator === undefined) {
+            return;
+        } else if (currentOperator != undefined && typeof(firstValue) === "object") {
+            firstValue = firstValue.join("");
+            secondValue = secondValue.join("");
+            firstValue = parseFloat(firstValue);
+            secondValue = parseFloat(secondValue);
+            equals(firstValue, secondValue, currentOperator);
+            currentOperator = element.value;
+            secondValue = [];
+ 
+        } else if (typeof(firstValue) === "number" && typeof(secondValue) === "object") {
+            secondValue = secondValue.join("");
+            secondValue = parseFloat(secondValue);
+            equals(firstValue, secondValue, currentOperator);
+            currentOperator = element.value;
+            secondValue = [];
+        }
+
+    });
+}))
+
